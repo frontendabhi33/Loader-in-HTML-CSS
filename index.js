138596d2916775798989ed881488664dc0f74693
Abhishek Rana <frontendabhi33@gmail.com>
2023-06-02 22:25:07 +0530

    Filterable Images

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ const filterButtons = document.querySelectorAll(".filter_buttons button");
 const filterableCards = document.querySelectorAll(".filterable_cards .card");
 
 const filterCards = (e) => {
-    document.querySelector(".active").classList.remove("active");
-    e.target.classList.add("active")
-    console.log(e);
-    filterableCards.forEach(card => {
-        card.classList.add("hide");
-        if(card.dataset.name === e.target.dataset.name || e.target.dataset.name === "all"){
-            card.classList.remove("hide")
-        }
-    })
-
+ document.querySelector(".active").classList.remove("active");
+ e.target.classList.add("active");
+ filterableCards.forEach(card => {
+    card.classList.add("hide");
+    if(card.dataset.name === e.target.dataset.name || e.target.dataset.name === "all") {
+        card.classList.remove("hide")
+    }
+ })
 }
 
-filterButtons.forEach(button => button.addEventListener("click", filterCards))
+filterButtons.forEach(button =>{
+    button.addEventListener("click", filterCards)
+})
