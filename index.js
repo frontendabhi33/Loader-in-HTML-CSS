427011353a136943916a414afd6f716d8addd2a9
Abhishek Rana <frontendabhi33@gmail.com>
2023-09-23 16:58:31 +0530

    first commit

--- index.js
+++ index.js
@@ -1,23 +1,65 @@
-const wrapper = document.querySelector(".wrapper"),
-qrInput = wrapper.querySelector(".form input"),
-generateBtn = wrapper.querySelector(".form button"),
-qrImg = wrapper.querySelector(".qr-code img")
+const fromText = document.querySelector(".from-text"),
+toText = document.querySelector(".to-text"),
+selectTag = document.querySelectorAll("select"),
+exchangeIcon = document.querySelector(".exchange")
+translateBtn = document.querySelector("button"),
+icons = document.querySelectorAll(".row i");
 
-generateBtn.addEventListener("click", () => {
-    let qrValue = qrInput.value;
-    if(!qrValue) return;
-    generateBtn.innerText = "Generating QR Code..."
-    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=170x170&data=${qrValue}`
-    qrImg.addEventListener("load", () => {
-        wrapper.classList.add("active");
-    generateBtn.innerText = "Generate QR Code"
 
-    })
+
+selectTag.forEach((tag,id) => {
+    for(const country_code in countries) {
+        let selected;
+        if(id == 0 && country_code == "en-GB"){
+            selected = "selected"
+        } else if(id == 1 && country_code == "hi-IN") {
+            selected = "selected"
+        }
+        let option = `<option value="${country_code}" ${selected}>${countries[country_code]}</option>`
+        tag.insertAdjacentHTML("beforeend", option)
+    }
 })
 
-qrInput.addEventListener("keyup", () => {
-    if(!qrInput.value) {
-        wrapper.classList.remove("active");
+exchangeIcon.addEventListener("click", () => {
+    let tempText = fromText.value,
+    tempLang = selectTag[0].value
+    fromText.value = toText.value;
+    selectTag[0].value = selectTag[1].value;
+    selectTag[1].value = tempLang
+    toText.value = tempText
+})
 
-    }
+translateBtn.addEventListener("click", () => {
+   let text = fromText.value,
+   translateFrom = selectTag[0].value,
+   translateTo = selectTag[1].value
+   if(!text) return;
+   toText.setAttribute("placeholder", "Translating...")
+   let apiUrl = `https://api.mymemory.translated.net/get?q=${text}!&langpair=${translateFrom}|${translateTo}`;
+   fetch(apiUrl).then(res => res.json()).then(data => {
+    console.log(data)
+      toText.value = data.responseData.translatedText;
+   })
+})
+
+icons.forEach(icon => {
+    icon.addEventListener("click", ({target}) => {
+       if(target.classList.contains("fa-copy")){
+         if(target.id == "from") {
+            navigator.clipboard.writeText(fromText.value)
+         } else {
+            navigator.clipboard.writeText(toText.value)
+         }
+       } else {
+        let utterance;
+        if(target.id == "from") {
+            utterance = new SpeechSynthesisUtterance(fromText.value)
+             utterance.lang = selectTag[0].value
+         } else {
+            utterance = new SpeechSynthesisUtterance(toText.value)
+             utterance.lang = selectTag[1].value
+         }
+         speechSynthesis.speak(utterance)
+       }
+    })
 })
