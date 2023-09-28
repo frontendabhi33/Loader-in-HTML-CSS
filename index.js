d20dd9b49c77aa3c23c096d21a7393c665b68a3c
Abhishek Rana <frontendabhi33@gmail.com>
2023-09-28 11:07:34 +0530

    first

--- index.js
+++ index.js
@@ -1,65 +1,35 @@
-const fromText = document.querySelector(".from-text"),
-toText = document.querySelector(".to-text"),
-selectTag = document.querySelectorAll("select"),
-exchangeIcon = document.querySelector(".exchange")
-translateBtn = document.querySelector("button"),
-icons = document.querySelectorAll(".row i");
+const screenshotBtn = document.querySelector("#src-btn"),
+screenshotPreview = document.querySelector(".src-preview"),
+closeBtn = screenshotPreview.querySelector("#close-btn");
 
+const captureScreen = async () => {
+    try {
+        // asking permission to use a media input to record current tab
+        const stream = await navigator.mediaDevices.getDisplayMedia({ preferCurrentTab: true });
+        const video = document.createElement("video");
+        
+        video.addEventListener("loadedmetadata", () => {
+            const canvas = document.createElement("canvas");
+            const ctx = canvas.getContext("2d");
 
-
-selectTag.forEach((tag,id) => {
-    for(const country_code in countries) {
-        let selected;
-        if(id == 0 && country_code == "en-GB"){
-            selected = "selected"
-        } else if(id == 1 && country_code == "hi-IN") {
-            selected = "selected"
-        }
-        let option = `<option value="${country_code}" ${selected}>${countries[country_code]}</option>`
-        tag.insertAdjacentHTML("beforeend", option)
+            // passing video width & height as canvas width & height
+            canvas.width = video.videoWidth;
+            canvas.height = video.videoHeight;
+            
+            video.play(); // playing the video so the drawn image won't be black or blank
+            // drawing an image of the captured video stream
+            ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+            stream.getVideoTracks()[0].stop(); // terminating first video track of the stream
+            
+            // passing canvas data url as screenshot preview src
+            screenshotPreview.querySelector("img").src = canvas.toDataURL();
+            screenshotPreview.classList.add("show");
+        });
+        video.srcObject = stream; // passing capture stream data as video source object
+    } catch (error) { // if image couldn't capture by any reason, then alert the msg
+        alert("Failed to capture screenshot!");
     }
-})
+}
 
-exchangeIcon.addEventListener("click", () => {
-    let tempText = fromText.value,
-    tempLang = selectTag[0].value
-    fromText.value = toText.value;
-    selectTag[0].value = selectTag[1].value;
-    selectTag[1].value = tempLang
-    toText.value = tempText
-})
-
-translateBtn.addEventListener("click", () => {
-   let text = fromText.value,
-   translateFrom = selectTag[0].value,
-   translateTo = selectTag[1].value
-   if(!text) return;
-   toText.setAttribute("placeholder", "Translating...")
-   let apiUrl = `https://api.mymemory.translated.net/get?q=${text}!&langpair=${translateFrom}|${translateTo}`;
-   fetch(apiUrl).then(res => res.json()).then(data => {
-    console.log(data)
-      toText.value = data.responseData.translatedText;
-   })
-})
-
-icons.forEach(icon => {
-    icon.addEventListener("click", ({target}) => {
-       if(target.classList.contains("fa-copy")){
-         if(target.id == "from") {
-            navigator.clipboard.writeText(fromText.value)
-         } else {
-            navigator.clipboard.writeText(toText.value)
-         }
-       } else {
-        let utterance;
-        if(target.id == "from") {
-            utterance = new SpeechSynthesisUtterance(fromText.value)
-             utterance.lang = selectTag[0].value
-         } else {
-            utterance = new SpeechSynthesisUtterance(toText.value)
-             utterance.lang = selectTag[1].value
-         }
-         speechSynthesis.speak(utterance)
-       }
-    })
-})
+closeBtn.addEventListener("click", () => screenshotPreview.classList.toggle("show"));
+screenshotBtn.addEventListener("click", captureScreen);
